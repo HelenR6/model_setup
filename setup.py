c3912f0c4be41007cb912f1994758015d8dbb192
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-04-13 12:40:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     url = '',
     description = '',
     packages = find_packages(),
-    install_requires = [
-    'robustness @ git+ssh://git@github.com/HelenR6/robustness@1.2.1',
-#     'clip @ git+ssh://git@github.com/openai/CLIP.git',
-#     'advertorch @ git+ssh://git@github.com/HelenR6/advertorch',
-]
+#     install_requires = [
+#     'robustness @ git+ssh://git@github.com/HelenR6/robustness@1.2.1',
+# #     'clip @ git+ssh://git@github.com/openai/CLIP.git',
+# #     'advertorch @ git+ssh://git@github.com/HelenR6/advertorch',
+# ]
  
 )
