82ee5b33199746eed090a74b24ca53d58f9174db
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-04-13 12:33:17 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'load_model',
+    version = '0.1.0',
+    url = '',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+    'robustness @ git+ssh://git@github.com/HelenR6/robustness',
+    'clip @ git+ssh://git@github.com/openai/CLIP.git',
+    'advertorch @ git+ssh://git@github.com/HelenR6/advertorch',
+]
+ 
+)
